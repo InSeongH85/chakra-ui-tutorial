553b177ff34f0d7c6fbef79cc9035e2e91fa00b9
InSeongH85 <tanbbang01@gmail.com>
2021-02-23 10:15:29 +0900

    First init chakra-ui

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import {
   Text,
   Link,
   VStack,
+  HStack,
+  StackDivider,
   Code,
   Grid,
   theme,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
+import SampleAirbnb from './SampleAirbnb';
 import { Logo } from './Logo';
 
 function App() {
@@ -18,21 +21,9 @@ function App() {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+          <Box bg="tomato" w="100%" p={4} color="white">
+            This is the Box
+          </Box>
         </Grid>
       </Box>
     </ChakraProvider>
