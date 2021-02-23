64122fdbfb96a255aa34987652c6f82727b2921b
InSeongH85 <tanbbang01@gmail.com>
2021-02-23 13:24:53 +0900

    Using Stack

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  HStack,
-  StackDivider,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
+import { ChakraProvider, Box, Grid, theme, HStack } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
-import SampleAirbnb from './SampleAirbnb';
-import { Logo } from './Logo';
-
+import PersonalStack from './PersonalStack';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <Box bg="tomato" w="100%" p={4} color="white">
-            This is the Box
-          </Box>
-        </Grid>
-      </Box>
+      <ColorModeSwitcher></ColorModeSwitcher>
+      <PersonalStack></PersonalStack>
     </ChakraProvider>
   );
 }
---
+++ src/PersonalStack.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Heading, HStack, Box, Text, Spacer } from '@chakra-ui/react';
+
+const PersonalStack = props => {
+  function Feature({ title, desc, ...rest }) {
+    return (
+      <Box
+        p={5}
+        shadow="md"
+        borderWidth="1px"
+        flex="1"
+        borderRadius="md"
+        {...rest}
+      >
+        <Heading fontSize="xl">{title}</Heading>
+        <Text mt={4}>{desc}</Text>
+      </Box>
+    );
+  }
+  return (
+    <HStack spacing={1}>
+      <Feature
+        title="Plan Money"
+        desc="The future can be even brighter but a goal without a plan is just a wish"
+      />
+      <Spacer></Spacer>
+      <Feature title="Save Money" desc="You deserve good things. " />
+    </HStack>
+  );
+};
+
+export default PersonalStack;
